ac8086608173d1953f045a3a8e570ba44a692c8e
Jake Lamb <jacoblamb@Jakes-MacBook-Pro.local>
2026-03-30 13:32:51 -0400

    add top nav to login page

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,17 +1,11 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import TopNav from '@/components/TopNav'
 
-const HIDE_NAV_PATHS = new Set(['/login'])
-
 export default function AppShell({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-  const showNav = pathname == null || !HIDE_NAV_PATHS.has(pathname)
-
   return (
     <>
-      {showNav && <TopNav />}
+      <TopNav />
       {children}
     </>
   )
