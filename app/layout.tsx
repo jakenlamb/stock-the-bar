87f6484137dbe39a7a88b27039abb4416ea4ffcf
Jake <145591246+jakenlamb@users.noreply.github.com>
2026-03-29 20:28:01 -0400

    Update page title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Stock the Bar',
-  description: 'A wedding alcohol registry',
+  title: 'Pour The Occasion',
+  description: 'A wedding & event alcohol registry',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
